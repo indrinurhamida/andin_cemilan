7aa34e459e6e022b6d13248866d18da5e26cd375
wahidahaddini <45058493+wahidahaddini@users.noreply.github.com>
2019-10-04 21:37:00 +0700

    hem

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -11,10 +11,6 @@ class Barang extends CI_Controller
 	public function index()
 	{
 		
-		$data['barang'] = $this->m_barang->tampil_data()->result();
-        $this->load->view('templates/header');
-        $this->load->view('templates/navbar');
-        $this->load->view('barang/daftarBarang', $data);
         $this->load->view('templates/footer');
 	}
 
