85aa7c8c374a96135f47906624027dd05fc58034
wahidahaddini <45058493+wahidahaddini@users.noreply.github.com>
2019-10-07 14:44:32 +0700

    betul

--- application/controllers/data-master/Barang.php
+++ application/controllers/data-master/Barang.php
@@ -14,21 +14,92 @@ class Barang extends CI_Controller
 		$data['barang'] = $this->m_barang->tampil_data()->result();
         $this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('data-master/barang/daftar_barang',$data);
+        $this->load->view('data-master/barang/daftarbarang',$data);
         $this->load->view('template/footer');
 	}
 
 	function tambah(){
 		$data['kode'] = $this->m_barang->tampil_kode();
+		$data['barangdetail'] = $this->m_barang->tampil_barangdetail()->result();
+
 		$this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('barang/daftar_barang',$data);
+        $this->load->view('data-master/barang/tambahbarang', $data);
         $this->load->view('template/footer');
 
 	}
 
-	function store(){
+	function tambah_aksi(){
+		$id_barang		= $this->input->post('id_barang');
+		$nama_barang	= $this->input->post('nama_barang');
+		$stok			= $this->input->post('stok');
+		$harga			= $this->input->post('harga');
+		$berat			= $this->input->post('berat');
+		$rasa			= $this->input->post('rasa');
+		$deskripsi		= $this->input->post('deskripsi');
+		$id_barangdetail= $this->input->post('id_barangdetail');
+
+		$data = array(
+			'id_barang'			=> $id_barang,
+			'nama_barang' 		=> $nama_barang,
+			'stok' 				=> $stok,
+			'harga' 			=> $harga,
+			'berat' 			=> $berat,
+			'rasa' 				=> $rasa,
+			'deskripsi' 		=> $deskripsi,
+			'id_barangdetail' 	=> $id_barangdetail
+		);
+
+		$this->m_barang->input_data($data, 'tbl_barang');
+		redirect('barang/index');
 		
+	}
+
+	function hapus ($id=''){
+		$where = array('id_barang' => $id);
+		$this->m_barang->hapus_data($where, 'tbl_barang');
+		redirect('data-master/barang/index');
+	}
+
+	function edit(){
+		$where = array('id_barangdetail');
+		$data['barang'] = $this->m_barang->edit_data('tbl_barang', $where)->row_array();
+		$data['data_barangdetail'] = $this->m_barang->tampil_barangdetail()->result();
+
+		$this->load->view('template/header');
+        $this->load->view('template/navbar');
+        $this->load->view('data-master/barang/editbarang', $data);
+        $this->load->view('template/footer');
+	}
+
+	function update(){
+		$id_barang		= $this->input->post('id_barang');
+		$nama_barang	= $this->input->post('nama_barang');
+		$stok			= $this->input->post('stok');
+		$harga			= $this->input->post('harga');
+		$berat			= $this->input->post('berat');
+		$rasa			= $this->input->post('rasa');
+		$deskripsi		= $this->input->post('deskripsi');
+		$id_barangdetail= $this->input->post('id_barangdetail');
+
+		$data = array(
+			'id_barang'			=> $id_barang,
+			'nama_barang' 		=> $nama_barang,
+			'stok' 				=> $stok,
+			'harga' 			=> $harga,
+			'berat' 			=> $berat,
+			'rasa' 				=> $rasa,
+			'deskripsi' 		=> $deskripsi,
+			'id_barangdetail' 	=> $id_barangdetail
+		);
+
+		$where = array(
+			'id_barang' => $id
+		);
+
+		$this->m_barang->update_data($where, 'tbl_barang', $data);
+		redirect('barang/index');
+
 	}
 }
 
