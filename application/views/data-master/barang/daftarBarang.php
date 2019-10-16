70067d5e998064f718bad5730858d96d406adc62
wahidahaddini <45058493+wahidahaddini@users.noreply.github.com>
2019-10-16 10:30:33 +0700

    LALALA

--- application/views/data-master/barang/daftarBarang.php
+++ application/views/data-master/barang/daftarBarang.php
@@ -2,11 +2,11 @@
 <div class="container-fluid">
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Data Barang</h1>
+        <h1 class="h3 mb-0 text-gray-800">Daftar Barang</h1>
     </div>
     <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <a href="<?php echo base_url() . 'index.php/data-master/barang/tambah'; ?>" class="btn btn-primary">Tambah Data</a>
+        <a href="<?php echo base_url() . 'index.php/barang/tambah'; ?>" class="btn btn-primary">Tambah Data</a>
     </div>
     <div class="card-body">
             <div class="table-responsive">
@@ -14,10 +14,13 @@
                 <thead>
                     <tr style="text-align:center">
                             <th>ID Barang</th>
+                            <th>Detail Barang</th>
                             <th>Nama Barang</th>
                             <th>Stok</th>
+                            <th>Harga</th>
+                            <th>Berat</th>
+                            <th>Rasa</th>
                             <th>Deskripsi</th>
-                            <th>Detail Barang</th>
                             <th>Aksi</th>
                     </tr>
                 </thead>
@@ -28,10 +31,13 @@
 
                             <tr>
                                 <td><?= $item->id_barang ?></td>
+                                <td><?= $item->id_barangdetail ?></td>
                                 <td><?= $item->nama_barang ?></td>
                                 <td><?= $item->stok ?></td>
+                                <td><?= $item->harga ?></td>
+                                <td><?= $item->berat ?></td>
+                                <td><?= $item->rasa ?></td>
                                 <td><?= $item->deskripsi ?></td>
-                                <td><?= $item->id_barangdetail ?></td>
                                 <td>
                                     <a href="<?php echo base_url() . 'index.php/barang/edit'; ?>/<?= $item->id_barang ?>" 
                                             class="btn btn-success">Edit</a>
