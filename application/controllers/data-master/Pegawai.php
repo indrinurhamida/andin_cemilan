c0d1b2964a87b55687804627e2b1b70234bd2b11
berlianarohmahaminin <44019028+berlianarohmahaminin@users.noreply.github.com>
2019-10-30 11:33:01 +0700

    pegawai

--- application/controllers/data-master/Pegawai.php
+++ application/controllers/data-master/Pegawai.php
@@ -4,11 +4,11 @@ class Pegawai extends CI_Controller
 {
 	function __construct(){
 		parent::__construct();
-		$this->load->model('M_pegawai');
+		$this->load->model('m_pegawai');
 	}
 
 	function index(){
-		$data['pegawai'] = $this->M_pegawai->tampil_data()->result();
+		$data['pegawai'] = $this->m_pegawai->tampil_data()->result();
     	$this->load->view('template/header');
     	$this->load->view('template/navbar');
        	$this->load->view('data-master/pegawai/daftarpegawai',$data);
@@ -16,7 +16,7 @@ class Pegawai extends CI_Controller
 	}
 
 	function tambah(){
-		$data['pegawai'] = $this->M_pegawai->tambah()->result();
+		$data['pegawai'] = $this->m_pegawai->tambah()->result();
 		$this->load->view('template/header');
         $this->load->view('template/navbar');
         $this->load->view('data-master/pegawai/tambahpegawai', $data);
@@ -39,21 +39,21 @@ class Pegawai extends CI_Controller
 	'no_hp' 			=> $no_hp,
 	);
 
-	$this->M_pegawai->input_data($data, 'tbl_pegawai');
+	$this->m_pegawai->input_data($data, 'tbl_pegawai');
 	redirect('data-master/pegawai');
 		
 	}
 
 	function hapus ($id=''){
 		$where = array('id_pegawai' => $id);
-		$this->M_pegawai->hapus_data($where, 'tbl_pegawai');
+		$this->m_pegawai->hapus_data($where,'tbl_pegawai');
 		redirect('data-master/pegawai');
 	}
 
 	function edit(){
 		$where = array('id_pegawai');
-		$data['pegawai'] = $this->M_pegawai->edit_data('tbl_pegawai', $where)->row_array();
-		$data['data_pegawai'] = $this->M_pegawai->tampil_pegawai()->result();
+		$data['pegawai'] = $this->m_pegawai->edit_data('tbl_pegawai', $where)->row_array();
+		$data['data_pegawai'] = $this->m_pegawai->tampil_pegawai()->result();
 
 		$this->load->view('template/header');
         $this->load->view('template/navbar');
@@ -82,7 +82,7 @@ class Pegawai extends CI_Controller
 	'id_pegawai' => $id
 	);
 
-	$this->M_pegawai->update_data($where, 'tbl_pegawai', $data);
+	$this->m_pegawai->update_data($where, 'tbl_pegawai', $data);
 	redirect('pegawai/index');
 
 	}
