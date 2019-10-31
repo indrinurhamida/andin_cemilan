458af59c6f4f326a22eb375689135c942d5ed0d1
[indrinurhamida] <[indrihamida7@gmail.com]>
2019-10-31 21:27:11 +0700

    tambah member

--- application/controllers/data-master/Member.php
+++ application/controllers/data-master/Member.php
@@ -17,68 +17,72 @@ class Member extends CI_Controller
 	}
 
 	function tambah(){
-		$data['kode'] = $this->m_supplier->tampil_kode();
+		$data['kode'] = $this->m_member->tampil_kode();
 
 		$this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('data-master/supplier/tambahsupplier', $data);
+        $this->load->view('data-master/member/tambahmember', $data);
         $this->load->view('template/footer');
 
 	}
 
 	function tambah_aksi(){
-		$id_supplier		= $this->input->post('id_supplier');
-		$nama_supplier		= $this->input->post('nama_supplier');
-		$alamat_supplier	= $this->input->post('alamat_supplier');
+		$id_member			= $this->input->post('id_member');
+		$nama_member		= $this->input->post('nama_member');
+		$email 				= $this->input->post('email');
+		$alamat 			= $this->input->post('alamat');
 		$no_hp				= $this->input->post('no_hp');
 
 		$data = array(
-			'id_supplier'			=> $id_supplier,
-			'nama_supplier' 		=> $nama_supplier,
-			'alamat_supplier' 		=> $alamat_supplier,
-			'no_hp' 				=> $no_hp
+			'id_member'			=> $id_member,
+			'nama_member' 		=> $nama_member,
+			'email'				=> $email,
+			'alamat' 			=> $alamat,
+			'no_hp' 			=> $no_hp
 		);
 
-		$this->m_supplier->input_data($data, 'tbl_supplier');
-		redirect('data-master/supplier');
+		$this->m_member->input_data($data, 'tbl_member');
+		redirect('data-master/member');
 		
 	}
 
 	function hapus ($id=''){
-		$where = array('id_supplier' => $id);
-		$this->m_supplier->hapus_data($where, 'tbl_supplier');
-		redirect('data-master/supplier');
+		$where = array('id_member' => $id);
+		$this->m_member->hapus_data($where, 'tbl_member');
+		redirect('data-master/member');
 	}
 
 	function edit($id){
-		$where = array('id_supplier' => $id);
-		$data['supplier'] = $this->m_supplier->edit_data('tbl_supplier', $where)->row_array();
+		$where = array('id_member' => $id);
+		$data['member'] = $this->m_member->edit_data('tbl_member', $where)->row_array();
 
 		$this->load->view('template/header');
         $this->load->view('template/navbar');
-        $this->load->view('data-master/supplier/editSupplier', $data);
+        $this->load->view('data-master/member/editSupplier', $data);
         $this->load->view('template/footer');
 	}
 
 	function update(){
-		$id_supplier		= $this->input->post('id_supplier');
-		$nama_supplier		= $this->input->post('nama_supplier');
-		$alamat_supplier	= $this->input->post('alamat_supplier');
+		$id_member			= $this->input->post('id_member');
+		$nama_member		= $this->input->post('nama_member');
+		$email 				= $this->input->post('email');
+		$alamat				= $this->input->post('alamat');
 		$no_hp				= $this->input->post('no_hp');
 
 		$data = array(
-			'id_supplier'			=> $id_supplier,
-			'nama_supplier' 		=> $nama_supplier,
-			'alamat_supplier' 		=> $alamat_supplier,
-			'no_hp' 				=> $no_hp
+			'id_member'			=> $id_member,
+			'nama_member' 		=> $nama_member,
+			'email'				=> $email,
+			'alamat' 			=> $alamat,
+			'no_hp' 			=> $no_hp
 		);
 
 		$where = array(
-			'id_supplier' => $id_supplier
+			'id_member' => $id_member
 		);
 
-		$this->m_supplier->update_data($where, $data, 'tbl_supplier');
-		redirect('data-master/supplier');
+		$this->m_member->update_data($where, $data, 'tbl_member');
+		redirect('data-master/member');
 
 	}
 }
